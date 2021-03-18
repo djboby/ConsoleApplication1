0d7a21bd7bcbe63b141c473d3d2d388bfe45319d
SID1EGR <SID1EGR@EGRZ0008>
2021-03-18 12:52:28 +0100

    commit2

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("elso commit");
+            Console.WriteLine("masodik commit");
         }
     }
 }
